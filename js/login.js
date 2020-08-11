5b82809597ca76d360f32fb5a3d3e1151dc62a38
carolinalopezb <carolinalopezb@users.noreply.github.com>
2020-08-11 16:33:42 -0300

    entrega 1 punto 3 login html y js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,36 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+// document.addEventListener("DOMContentLoaded", function(e){
+//});
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("submitBtn").addEventListener("click",function(e){
+
+        let inputUser = document.getElementById("inputUser");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+
+        if (inputUser.value === ''){
+          inputUser.classList.add("invalid" );
+          camposCompletos = false;
+          
+        }
+        
+        if (inputPassword.value ===''){
+            inputPassword.classList.add("invalid");
+            camposCompletos = false;
+        }
+        if (camposCompletos) {
+            
+                        window.location = 'index.html';
+                
+        }else{
+
+            alert("Debes ingresar los datos!")
+        }
+        
+    });
 });
