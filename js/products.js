f3d9253f23d863715c556eb17c2901b7b8d0e8ce
carolinalopezb <carolinalopezb@users.noreply.github.com>
2020-08-11 15:16:24 -0300

    Entrega 1 punto 2

--- js/products.js
+++ js/products.js
@@ -1,6 +1,35 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+var productsArray = [];
+
+let htmlContentToAppend = "";
+function showProducts (array){
+
+    for (let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        
+        Nombre: `+ product.name +` 
+        <br>
+        Precio: `+ product.currency + product.cost + `
+        <br>
+        Descripción: `+ product.description + `
+        <br>
+        <br>
+        `
+
+        document.getElementById("prod").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener ("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then (function(resultObj){
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProducts(productsArray);
+        }
+    });
 });
