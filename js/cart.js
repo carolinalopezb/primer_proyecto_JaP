1e32da08e140d807278483936da24ec6abae252d
carolinalopezb <carolinalopezb@users.noreply.github.com>
2020-10-11 14:00:42 -0300

    entrega 5 y desafiate carrito, boton dolar peso no funciona

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,103 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+
+var cartArray = [];
+var moneda = "USD";
+
+
+//Cambiar dolar a peso y viceversa --------------------------------------------------------------
+
+
+function dolarPeso() {
+    if (moneda === "USD") {
+        moneda = "UYU"
+
+    } else  if (moneda === "UYU") {
+        moneda = "USD"
+
+    } 
+   // showCart(cartArray);    No funciona.
+}
+// Calcular total productos------------------------------------------------------------------------
+     
+function Total(){
+    let total = 0;
+    let subtotales = document.getElementsByClassName("subtotal");
+    for (let i = 0; i < subtotales.length; i++){
+        total += parseInt(subtotales[i].innerHTML);
+    }
+    document.getElementById("total").innerHTML = total;
+    
+}
+
+// Calcular subtotal de cada producto--------------------------------------------------------------
+function Subtotal(precio, i){
+
+    let cantidad = parseInt(document.getElementById(`cantidad${i}`).value);
+    subtotal = cantidad * precio; 
+    document.getElementById(`productSubtotal${i}`).innerHTML = subtotal;
+    Total();
+}
+
+// Mostrar productos y carrito ---------------------------------------------------------------------
+
+function showCart(array) {
+
+    let contenido = "";
+
+    for (let i = 0; i < array.length; i++) {
+        
+        let product = array[i];
+
+        let moneda2 = moneda;
+
+        if (moneda === "USD" && product.currency === "UYU") {
+            product.unitCost = product.unitCost / 40;
+        }
+
+        if (moneda === "UYU" && product.currency === "USD") {
+            product.unitCost = product.unitCost * 40;
+        }
+
+
+        let sub = product.unitCost * product.count;
+
+        contenido += `
+        <tr>
+            <td><img src='${product.src}' width="50px"></td>
+
+            <td>'${product.name}'</td>
+
+            <td><input style="width:60px;" onchange="Subtotal(${product.unitCost}, ${i})" 
+                type="number" id="cantidad${i}" value="${product.count}" min="1"></td>
+
+            <td >${moneda2}</td>
+
+            <td >${ product.unitCost}</td>
+
+            <td><span class="subtotal" id="productSubtotal${i}" style="font-weight:bold;">${sub}</span></td>
+            
+            
+        </tr>
+        `
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+    Total()
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(CART_INFO_URL_2).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            cartArray = resultObj.data.articles;
+
+            
+            showCart(cartArray);
+
+        }
+
+    });
+    
 
 });
